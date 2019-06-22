398d675511dc5f259e06d50abcdd77bc63acecb7
Jim Hessin <jhessin@gmail.com>
2019-06-22 12:46:29 -0400

    Yet more shortcuts

---
+++ functions/fishconfig.fish
@@ -0,0 +1,3 @@
+function fishconfig
+vim ~/.config/fish/config.fish
+end
--- functions/shortcuts.fish
+++ functions/shortcuts.fish
@@ -1,3 +1,9 @@
+# EDIT THIS FILE QUICKLY
+function shortcuts
+vim ~/.config/fish/functions/shortcuts.fish
+end
+
+# shortcuts to common directories
 function github
 cd ~/Documents/github/$argv
 end
@@ -11,9 +17,26 @@ cd ~/Documents/github/notes/
 end
 
 function fishrc
-cd ~/.config/fish
+cd ~/.config/fish/$argv
 end
 
+# Reload the config file
 function reload
 source ~/.config/fish/config.fish
 end
+
+# some git shortcuts
+function gpush
+git add .
+git commit
+git push
+end
+
+function gpull
+git pull
+end
+
+function greset
+git reset --hard
+end
+
