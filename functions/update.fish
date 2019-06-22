cb5a5433f7fe0126f3c73d4179cbdc6930d21dcf
Jim Hessin <jhessin@gmail.com>
2019-06-22 13:51:15 -0400

    update should work now

--- functions/update.fish
+++ functions/update.fish
@@ -1,4 +1,20 @@
 function update
+  # bring in any additional repos and files
+  source $HOME/.config/fish/repos.fish
+  source $HOME/.config/fish/files.fish
+
+  # copy backup files
+  set target $HOME/.config/fish/backup
+  for file in $files
+    cp $target/$file $HOME
+  end
+
+  for dir in $repos
+    pushdir $dir
+    gpull
+    popd
+  end
+
   for dir in $HOME/Documents/github/*
     pushd $dir
     gpull
