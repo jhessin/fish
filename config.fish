69ba856db165210a1a954bc4b9212f4eeaf86b47
Jim Hessin <jhessin@gmail.com>
2019-06-22 12:36:14 -0400

    Added shortcuts

--- config.fish
+++ config.fish
@@ -1,1 +1,2 @@
 fish_vi_key_bindings
+source $HOME/.config/fish/functions/shortcuts.fish
---
+++ functions/shortcuts.fish
@@ -0,0 +1,19 @@
+function github
+cd ~/Documents/github/$argv
+end
+
+function bitbucket
+cd ~/Documents/bitbucket/$argv
+end
+
+function notes
+cd ~/Documents/github/notes/
+end
+
+function fishrc
+cd ~/.config/fish
+end
+
+function reload
+source ~/.config/fish/config.fish
+end
