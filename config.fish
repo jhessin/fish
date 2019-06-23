92a5223fbdce71bdbf5c04a4e9da4630f7f74c08
Jim Hessin <jhessin@gmail.com>
2019-06-23 17:39:46 -0400

    Created a setup file

--- config.fish
+++ config.fish
@@ -1,7 +1,8 @@
 source $HOME/.config/fish/functions/shortcuts.fish
 set -g fish_key_bindings fish_user_keybindings
+set -gx EDITOR nvim
 set -x PAGER "/bin/sh -c \"unset PAGER;col -b -x | \
     vim -R -c 'set ft=man nomod nolist' -c 'map q :q<CR>' \
     -c 'map <SPACE> <C-D>' -c 'map b <C-U>' \
     -c 'nmap K :Man <C-R>=expand(\\\"<cword>\\\")<CR><CR>' -\""
-
+set -gx PATH /home/linuxbrew/.linuxbrew/bin /home/jhessin/.local/bin $PATH
---
+++ setup.fish
@@ -0,0 +1,32 @@
+# See if we need git and download if necessary
+if not test (type git)
+  brew install git
+else
+  echo "git found"
+end
+
+# Get hub as a shortcut
+if not test (type hub)
+  brew install hub
+end
+
+# Set up curl if we don't have it
+if not test (type curl)
+  brew install curl
+end
+
+# ensure neovim is installed and set up
+if not test (type nvim)
+  brew install neovim
+ # set up spacevim
+  curl -sLf https://spacevim.org/install.sh | bash
+end
+
+# ensure my spacevim repo is cloned
+if not test -d ~/.SpaceVim.d
+  if not git clone git@bitbucket.org:jhessin/vim.git ~/.SpaceVim.d
+    echo "git clone failed - ensure you have your key set up with bitbucket"
+  end
+end
+
+update
