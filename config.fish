8fb5c8b1abb7496ecb92c8f6b2009be2a10eeb84
Jim Hessin <jhessin@gmail.com>
2019-06-24 11:06:54 -0600

    Added yarn, ruby, etc. to path.

--- config.fish
+++ config.fish
@@ -1,10 +1,27 @@
+# add shortcuts and tools
 source $HOME/.config/fish/functions/shortcuts.fish
+
+# custom bindings to work with tmux
 set -g fish_key_bindings fish_user_keybindings
+
+# set the default editor to nvim
 set -gx EDITOR nvim
-set -x PAGER "/bin/sh -c \"unset PAGER;col -b -x | \
+
+# This is supposed to allow nvim to read man pages
+set -gx PAGER "/bin/sh -c \"unset PAGER;col -b -x | \
     vim -R -c 'set ft=man nomod nolist' -c 'map q :q<CR>' \
     -c 'map <SPACE> <C-D>' -c 'map b <C-U>' \
     -c 'nmap K :Man <C-R>=expand(\\\"<cword>\\\")<CR><CR>' -\""
-set -gx PATH /home/linuxbrew/.linuxbrew/bin /home/jhessin/.local/bin $PATH
+
 # if necessary set the keyboard layout
 # setxkbmap -layout us -variant dvp -option compose:102 -option keypad:atm -option numpad:shift3 -option kpdl:semi
+
+# Add homebrew and it's installs to the path
+set -gx PATH /home/linuxbrew/.linuxbrew/bin $PATH
+
+# Add ruby gems to the path
+set -gx PATH $PATH /home/jhessin/.local/bin 
+set -gx PATH $PATH /home/jhessin/.gem/ruby/2.3.0/bin 
+
+# Add yarn and it's installs to the path
+set -gx PATH $PATH /home/jhessin/.yarn/bin
