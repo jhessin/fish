e252a4e07314e6d95c526591a87e8756d5624d81
Jim Hessin <jhessin@gmail.com>
2019-07-26 14:03:36 -0600

    Now updates fish before reading repos

--- functions/update.fish
+++ functions/update.fish
@@ -1,11 +1,11 @@
 function update
-  # bring in any additional repos and files
-  source $HOME/.config/fish/repos.fish
 
   # first update this repo
   cd ~/.config/fish
   gpull
 
+  # bring in any additional repos and files
+  source $HOME/.config/fish/repos.fish
   # copy backup files
   set target $HOME/.config/fish/backup
   # keep an eye on this - if I add anything that isn't proceeded by a dot it could be tricky
