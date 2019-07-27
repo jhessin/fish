0fbcdbe68b172c9e869871c8e7514835ef6fc5c7
Jim Hessin <jhessin@gmail.com>
2019-07-27 12:02:06 -0600

    fixed display layout

--- functions/disp-hd.fish
+++ functions/disp-hd.fish
@@ -1,4 +1,4 @@
 function disp-hd
-  xrandr --output VGA-0 --primary --auto --output DVI-0 --right-of VGA-0 --auto
+  xrandr --output VGA-0 --primary --auto --pos 0x0 --output DVI-0 --pos 1024x-312
 end
 
