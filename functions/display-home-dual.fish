d4ca7d1f9349cb76f0df904677e190ebe917ef3c
Jim Hessin <jhessin@gmail.com>
2019-07-27 08:56:33 -0600

    Created function shortcuts for managing monitors

---
+++ functions/display-home-dual.fish
@@ -0,0 +1,4 @@
+function display-home-dual
+  xrandr --output VGA-0 --primary --auto --output DVI-0 --right-of VGA-0 --auto
+end
+
---
+++ functions/display-home-single.fish
@@ -0,0 +1,4 @@
+function display-home-single
+  xrandr --output VGA-0 --off --output DVI-0 --primary --auto
+end
+
