a1b5a7c63c5fa7691342dd1a32193f4756b68c6a
Jim Hessin <jhessin@gmail.com>
2019-07-27 07:55:42 -0600

    setup default monitor setup

--- functions/mountbackups.fish
+++ functions/mountbackups.fish
@@ -1,9 +1,11 @@
 function mountbackups
+  fusermount -u /media/backups
   if test (count $argv) -eq 0
     set argv /dev/sdb2
-  end
-  sudo apfs-fuse -o allow_other $argv /media/backups
+end
+if sudo apfs-fuse -o allow_other $argv /media/backups
   cd /media/backups/root/
   cp -rv * ~/harddrive/
+end
 end
 
