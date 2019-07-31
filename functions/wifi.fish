5799d9cd8792999b4c90c4d464131656fec5c682
Jim Hessin <jhessin@gmail.com>
2019-07-31 09:48:07 -0600

    can now disconnect iphone with wifi -iphone

--- functions/wifi.fish
+++ functions/wifi.fish
@@ -1,12 +1,14 @@
 function wifi
   if test $argv = ls
     nmcli d wifi list
-else if test $argv = iphone
-  if ! nmcli c up iPhone
-    nmcli d wifi connect iPhone password ocokw7qc04nc4
-end
-else
-  nmcli d wifi $argv
-end
+  else if test $argv = iphone
+    if ! nmcli c up iPhone
+      nmcli d wifi connect iPhone password ocokw7qc04nc4
+    end
+  else if test $argv = -iphone
+    nmcli c down iPhone
+  else
+    nmcli d wifi $argv
+  end
 end
 
