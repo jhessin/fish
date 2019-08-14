80ae764866ab624a8d64dd795c0857047df03621
Jim Hessin <jhessin@gmail.com>
2019-08-14 11:03:34 -0600

    Outsourced most of the updating

--- functions/update.fish
+++ functions/update.fish
@@ -6,66 +6,11 @@ function update_this
   end
 end
 
-function copy_files
-  # bring in any additional repos and files
-  source $HOME/.config/fish/repos.fish
-  # copy backup files
-  set target $HOME/.config/fish/backup
-  # keep an eye on this - if I add anything that isn't proceeded by a dot it could be tricky
-  for file in $target/.*
-    cp -v $file $HOME
-  end
-
-  for dir in $repos
-    if pushd $dir
-      echo "updating $dir"
-      gpull
-      popd
-    end
-  end
-
-  if test (uname) = 'Linux'
-    for dir in $linuxRepos
-      if pushd $dir
-        echo "updating $dir"
-        gpull
-        popd
-      end
-    end
-  end
-
-  for dir in $HOME/Documents/github/*
-    if pushd $dir
-      echo "updating $dir"
-      gpull
-      popd
-    end
-  end
-
-  for dir in $HOME/Documents/bitbucket/*
-    if pushd $dir
-      echo "updating $dir"
-      gpull
-      popd
-    end
-  end
-end
-
-function install_packages
-  # install any missing packages
-  if test (uname) = 'Linux'
-    echo "Installing Packages"
-    yay -S --needed --noconfirm - < $linuxPackages
-  else
-    cat $macBrewList | xargs brew install 2> /dev/null
-    cat $macBrewList | xargs brew upgrade 2> /dev/null
-  end
-end
-
 function update
   update_this
 
   source ~/.config/fish/files.fish
+  source ~/.config/fish/update.fish
 
   copy_files
   install_packages
---
+++ update.fish
@@ -0,0 +1,58 @@
+
+function copy_files
+  # bring in any additional repos and files
+  source $HOME/.config/fish/repos.fish
+  # copy backup files
+  set target $HOME/.config/fish/backup
+  # keep an eye on this - if I add anything that isn't proceeded by a dot it could be tricky
+  for file in $target/.*
+    cp -v $file $HOME
+  end
+
+  for dir in $repos
+    if pushd $dir
+      echo "updating $dir"
+      gpull
+      popd
+    end
+  end
+
+  if test (uname) = 'Linux'
+    for dir in $linuxRepos
+      if pushd $dir
+        echo "updating $dir"
+        gpull
+        popd
+      end
+    end
+  end
+
+  for dir in $HOME/Documents/github/*
+    if pushd $dir
+      echo "updating $dir"
+      gpull
+      popd
+    end
+  end
+
+  for dir in $HOME/Documents/bitbucket/*
+    if pushd $dir
+      echo "updating $dir"
+      gpull
+      popd
+    end
+  end
+end
+
+function install_packages
+  # install any missing packages
+  if test (uname) = 'Linux'
+    echo "Installing Packages"
+    yay -S --needed --noconfirm - < $linuxPackages
+  else
+    cat $macBrewList | xargs brew install 2> /dev/null
+    cat $macBrewList | xargs brew upgrade 2> /dev/null
+  end
+end
+
+
