5e683f949bf5862141743595366551a19747ac2d
Jim Hessin <jhessin@gmail.com>
2019-08-14 21:13:35 -0600

    Added dmenu-recent settings

--- repos.fish
+++ repos.fish
@@ -1,4 +1,5 @@
 set -x repos $HOME/.SpaceVim.d
+set -x repos $repos $HOME/.config/dmenu-recent
 set -x repos $repos $HOME/.config/powerline
 
 set -x linuxRepos $HOME/.irssi
@@ -9,6 +10,11 @@ function setup_repos
   if not test -d $HOME/.SpaceVim.d/.git
     rm -rf $HOME/.SpaceVim.d
     git clone git@bitbucket.org:jhessin/vim.git $HOME/.SpaceVim.d
+  end
+
+  if not test -d $HOME/.config/dmenu-recent/.git
+    rm -rf $HOME/.config/dmenu-recent
+    hub clone jhessin/dmenu-recent $HOME/.config/dmenu-recent
   end
 
   if not test -d ~/.config/powerline/.git
