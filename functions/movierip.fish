05bbc9f150f801b06be65c18e1f61700f226689a
Jim Hessin <jhessin@gmail.com>
2019-08-18 12:27:57 -0600

    tvrip now copies to external drive

--- functions/movierip.fish
+++ functions/movierip.fish
@@ -1,5 +1,9 @@
 function movierip -a title
   mkdir -p $HOME/Videos/Movies
-  HandBrakeCLI -Z "H.264 MKV 720p30" --main-feature --subtitle-lang-list eng -i /dev/sr0 -o "$HOME/Videos/Movies/$title.mkv"
+  if set -q argv
+    HandBrakeCLI -Z "H.264 MKV 720p30" $argv --subtitle-lang-list eng -i /dev/sr0 -o "$HOME/Videos/Movies/$title.mkv"
+  else
+    HandBrakeCLI -Z "H.264 MKV 720p30" --main-feature --subtitle-lang-list eng -i /dev/sr0 -o "$HOME/Videos/Movies/$title.mkv"
+  end
 end
 
--- functions/tvrip.fish
+++ functions/tvrip.fish
@@ -1,11 +1,12 @@
 function tvrip -a show first last season episode 
   set title $first
   mkdir -p $HOME/Videos/TV/$show/Season_$season
-  while test $title -lt $last
+  set path /run/media/jhessin/backups/HOME/Videos/TV
+  while test $title -lt (math $last + 1)
     if test $episode -lt 10
-      HandBrakeCLI -Z "H.264 MKV 720p30" -t $title --subtitle-lang-list eng -i /dev/sr0 -o "$HOME/Videos/TV/$show/Season_$season/$show-S$season-E0$episode.mkv"
+      HandBrakeCLI -Z "H.264 MKV 720p30" -t $title --subtitle-lang-list eng -i /dev/sr0 -o "$path/$show/Season_$season/$show-S$season-E0$episode.mkv"
     else
-      HandBrakeCLI -Z "H.264 MKV 720p30" -t $title --subtitle-lang-list eng -i /dev/sr0 -o "$HOME/Videos/TV/$show/Season_$season/$show-S$season-E$episode.mkv"
+      HandBrakeCLI -Z "H.264 MKV 720p30" -t $title --subtitle-lang-list eng -i /dev/sr0 -o "$path/$show/Season_$season/$show-S$season-E$episode.mkv"
     end
     set title (math "$title + 1")
     set episode (math "$episode + 1")
