2542adc412b672ce1489f8b4d17fc8fdcf7bdce6
Jim Hessin <jhessin@gmail.com>
2019-08-21 08:54:21 -0600

    Redid default keybindings

--- functions/fish_user_keybindings.fish
+++ functions/fish_user_keybindings.fish
@@ -2,6 +2,6 @@ function fish_user_keybindings --description "vi bindings with tmux copy mode ca
   fish_vi_key_bindings
   bind -M visual v copy_mode
   bind p paste_buffer
-  bind -M insert \t forward-char
-  bind -M insert -k btab complete-and-search
+  # bind -M insert \t forward-char
+  # bind -M insert -k btab complete-and-search
 end
