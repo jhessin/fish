1a59b1aa7a08867c9889496dadab224f6e6e1d39
Jim Hessin <jhessin@gmail.com>
2019-09-10 06:47:43 -0600

    improving bbclone

--- functions/bbclone.fish
+++ functions/bbclone.fish
@@ -1,6 +1,8 @@
 function bbclone
   git clone git@bitbucket.org:$argv.git
-  cd $argv
+  set path (string split "/" $argv)
+  # echo $path[2]
+  cd $path[2]
   git push -u origin master
   cd ..
 end
