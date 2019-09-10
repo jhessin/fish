649574156ccf39ba9d8f4c04ff5930b856f50afb
Jim Hessin <jhessin@gmail.com>
2019-09-10 06:24:49 -0600

    Trying again to improve gfetch

--- functions/gfetch.fish
+++ functions/gfetch.fish
@@ -1,7 +1,9 @@
 function gfetch
-  git fetch origin $argv
-  git checkout "origin/$argv"
-  git checkout -b $argv
-  git push -u origin $argv
+  # git fetch origin $argv
+  git pull -t
+  # git checkout "origin/$argv"
+  # git checkout -b $argv
+  git checkout $argv
+  # git push -u origin $argv
 end
 
