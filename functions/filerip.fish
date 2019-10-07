02934ee555c7ccbd172c10ff03316d343cb7953a
Jim Hessin <jhessin@gmail.com>
2019-10-07 17:17:07 -0600

    updated

--- functions/filerip.fish
+++ functions/filerip.fish
@@ -1,11 +1,12 @@
 function filerip -a filename
   set temp (mktemp -d)
-  cp $filename $filename.bak
   set rootname (echo $filename | sed 's/\.[^.]*$//')
   set dest $rootname.mkv
-  if HandBrakeCLI -i $filename -Z "H.264 MKV 720p30" -o $temp/$dest
+  set src $rootname.bak
+  mv $filename $src
+  if HandBrakeCLI -i $src -Z "H.264 MKV 720p30" -o $temp/$dest
     cp -v $temp/$dest $dest
-    rm $filename.bak
+    rm $src
   else
     echo CANCELED
   end
