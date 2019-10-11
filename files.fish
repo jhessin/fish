a06487693a0d53aa0d263813790984e7d7444321
Jim Hessin <jhessin@gmail.com>
2019-10-11 12:17:49 -0600

    updated

--- files.fish
+++ files.fish
@@ -3,6 +3,7 @@ set -a files $HOME/.profile
 set -a files $HOME/.synergy.conf
 set -a files $HOME/.rtorrent.rc
 set -a files $HOME/.config/mimeapps.list
+set -a files $HOME/.abcde.conf
 set -ax files $HOME/.tmux.conf
 
 set -x backupDir $HOME/.config/fish/backup
