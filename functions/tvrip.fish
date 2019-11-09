5ede4e3443bb2a8bb956358c53453eb148f963da
Jim Hessin <jhessin@gmail.com>
2019-11-09 16:45:33 -0700

    updated

--- functions/tvrip.fish
+++ functions/tvrip.fish
@@ -4,9 +4,9 @@ function tvrip -a show first last season episode
   mkdir -p $path/$show/Season_$season
   while test $title -lt (math $last + 1)
     if test $episode -lt 10
-      HandBrakeCLI -Z "H.264 MKV 720p30" -t $title --subtitle-lang-list eng -i /dev/sr0 -o "$path/$show/Season_$season/$show-S$season-E0$episode.mkv"
+      HandBrakeCLI -Z "H.264 MKV 720p30" -t $title -s eng -a eng -i /dev/sr0 -o "$path/$show/Season_$season/$show-S$season-E0$episode.mkv"
     else
-      HandBrakeCLI -Z "H.264 MKV 720p30" -t $title --subtitle-lang-list eng -i /dev/sr0 -o "$path/$show/Season_$season/$show-S$season-E$episode.mkv"
+      HandBrakeCLI -Z "H.264 MKV 720p30" -t $title -s eng -a eng -i /dev/sr0 -o "$path/$show/Season_$season/$show-S$season-E$episode.mkv"
     end
     set title (math "$title + 1")
     set episode (math "$episode + 1")
